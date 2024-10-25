1f90976d25237954c9ae5d85a3d8beaef36be40f
Anas Farooq <anasfarooq2006@gmail.com>
2024-10-25 23:28:34 +0500

    Started Profile page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { Typography } from "antd";
 import { Icon } from "@iconify/react";
 import { usePathname, useRouter } from "next/navigation";
-import { useClerk } from "@clerk/nextjs";
+import { useClerk, useUser } from "@clerk/nextjs";
 import cx from "classnames";
 import { useSettingsContext } from "@/context/settings/settings-context";
 import SidebarContainer from "./SidebarContainer";
@@ -17,6 +17,8 @@ const Sidebar = () => {
   const { signOut } = useClerk();
   const pathname = usePathname();
   const [mounted, setMounted] = useState(false)
+
+  const {user} = useUser()
 
   useEffect(() => {
     setMounted(true)
@@ -47,10 +49,10 @@ const Sidebar = () => {
     setIsDrawerOpen = {handleDrawerClose}>
       <div className={css.wrapper}>
         <Box className={css.container}>
-          {sidebarRoutes().map((route, index) => (
+          {sidebarRoutes(user).map((route, index) => (
             <Link
               key={index}
-              href={route.route}
+              href={route.route === `/profile/${user?.id}` ? `${route.route}?person=${user?.firstName}` : `${route.route}`}
               className={cx(css.item, isActive(route))}
             >
               {/* Icon  */}
